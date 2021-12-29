ccbd82e53d0fba1de6e7b62b5d2a8e45a292fa58
2dsutrisno <2d.sutrisno@gmail.com>
2021-12-29 20:20:02 +0700

    Add files via upload

--- kkpy.py
+++ kkpy.py
@@ -5,6 +5,11 @@ import glob
 from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
+import parse_date
+
+
+
+
 
 class KKPy:
     def __init__(self, npwp, masa_awal, masa_akhir, thn_pajak):
@@ -16,17 +21,23 @@ class KKPy:
     
     def a1_to_n21(self, masa):
         print("copy ekspor masa " + masa)
-        path = os.getcwd() + "\Data\\"+ self.npwp +"\\SPT PPN MASA " + masa +" TAHUN PAJAK " + self.thn_pajak + "\SPT MASA PPN LAMPIRAN A1"
+
+        path = os.path.join(os.getcwd(), "Data", self.npwp,  "SPT PPN MASA " + masa +" TAHUN PAJAK " + self.thn_pajak, "SPT MASA PPN LAMPIRAN A1")
+        
         if os.path.exists(path):
-            filenames = glob.glob(path + "\*.csv")
+            filenames = glob.glob(path + os.sep + "*.csv")
             
             tipe_data = {
                 'ID_SPT':np.str,
                 'NO_FAKTUR':np.str
                 }
             df = pd.read_csv(filenames[0], sep=";", dtype=tipe_data)
-            siap_ekspor = df[['NAMA_PARTNER', 'NO_FAKTUR', 'TANGGAL_FAKTUR', 'JUMLAH_DPP', 'KET']]
-            siap_ekspor['MASA'] = int(masa)
+            df['MASA'] = int(masa)
+            df['TANGGAL_FAKTUR'] = df['TANGGAL_FAKTUR'].apply(lambda x: parse_date.parse(x))
+
+            siap_ekspor = df[['NAMA_PARTNER', 'NO_FAKTUR', 'TANGGAL_FAKTUR', 'JUMLAH_DPP', 'KET', 'MASA']].reset_index(drop=True)
+            siap_ekspor.index = siap_ekspor.index + 1
+
             ws = self.wb['N.2.1']
             for r in dataframe_to_rows(siap_ekspor, index=True, header=False):
                 ws.append(r)
@@ -36,9 +47,9 @@ class KKPy:
 
     def a2_to_n31(self, masa)    :
         print("copy dipungut sendiri masa " + masa)
-        path = os.getcwd() + "\Data\\"+ self.npwp +"\\SPT PPN MASA " + masa +" TAHUN PAJAK " + self.thn_pajak + "\SPT MASA PPN LAMPIRAN A2"
+        path = os.path.join(os.getcwd(), "Data", self.npwp ,"SPT PPN MASA " + masa +" TAHUN PAJAK " + self.thn_pajak, "SPT MASA PPN LAMPIRAN A2")
         if os.path.exists(path):
-            filenames = glob.glob(path + "\*.csv")
+            filenames = glob.glob(path + os.sep + "*.csv")
             tipe_data = {
                 'ID_SPT':np.str,
                 'NPWP_PARTNER':np.str,
@@ -50,12 +61,13 @@ class KKPy:
                 }
 
             df = pd.read_csv(filenames[0], sep=";", dtype=tipe_data)
-            df['TANGGAL_FAKTUR'] = pd.to_datetime(df['TANGGAL_FAKTUR'].str.split(', ', expand=True)[1])
-            
+            df['MASA'] = int(masa)
+            df['TANGGAL_FAKTUR'] = df['TANGGAL_FAKTUR'].apply(lambda x: parse_date.parse(x))
+
             df01 = df[df['KD_TRX'] == '01']
-            siap = df01[['NAMA_PARTNER', 'NPWP_PARTNER', 'NO_FAKTUR', 'TANGGAL_FAKTUR', 'JUMLAH_DPP', 'JUMLAH_PPN', 'JUMLAH_PPNBM', 'NO_FAKTUR_PENGGANTI']].reset_index(drop=True)
+            siap = df01[['NAMA_PARTNER', 'NPWP_PARTNER', 'NO_FAKTUR', 'TANGGAL_FAKTUR', 'JUMLAH_DPP', 'JUMLAH_PPN', 'JUMLAH_PPNBM', 'NO_FAKTUR_PENGGANTI', 'MASA']].reset_index(drop=True)
             siap.index = siap.index + 1
-            siap['MASA'] = int(masa)
+            
             ws = self.wb['N.3.1']
             for r in dataframe_to_rows(siap, index=True, header=False):
                 ws.append(r)
@@ -64,11 +76,11 @@ class KKPy:
             print("- masa " + masa + " tidak ada")
 
 
-    def a2_to_n41(self, masa)    :
-        print("copy dipungut sendiri masa " + masa)
-        path = os.getcwd() + "\Data\\"+ self.npwp +"\\SPT PPN MASA " + masa +" TAHUN PAJAK " + self.thn_pajak + "\SPT MASA PPN LAMPIRAN A2"
+    def a2_to_n51(self, masa)    :
+        print("copy tidak dipungut masa " + masa)
+        path = os.path.join(os.getcwd(),  "Data", self.npwp, "SPT PPN MASA " + masa +" TAHUN PAJAK " + self.thn_pajak, "SPT MASA PPN LAMPIRAN A2")
         if os.path.exists(path):
-            filenames = glob.glob(path + "\*.csv")
+            filenames = glob.glob(path + os.sep + "*.csv")
 
             tipe_data = {
                 'ID_SPT':np.str,
@@ -80,12 +92,15 @@ class KKPy:
                 'NO_FAKTUR_PENGGANTI':np.str
                 }
             df = pd.read_csv(filenames[0], sep=";", dtype=tipe_data)
-            df01 = df[df['KD_TRX'] == '01']
+            df['MASA'] = int(masa)
+            df['TANGGAL_FAKTUR'] = df['TANGGAL_FAKTUR'].apply(lambda x: parse_date.parse(x))
+
+            df01 = df[df['KD_TRX'] == '07']
         
-            siap = df01[['NAMA_PARTNER', 'NPWP_PARTNER', 'NO_FAKTUR', 'TANGGAL_FAKTUR', 'JUMLAH_DPP', 'JUMLAH_PPN', 'JUMLAH_PPNBM', 'NO_FAKTUR_PENGGANTI']].reset_index(drop=True)
+            siap = df01[['NAMA_PARTNER', 'NPWP_PARTNER', 'NO_FAKTUR', 'TANGGAL_FAKTUR', 'JUMLAH_DPP', 'JUMLAH_PPN', 'JUMLAH_PPNBM', 'NO_FAKTUR_PENGGANTI', 'MASA']].reset_index(drop=True)
             siap.index = siap.index + 1
-            ws = self.wb['N.4.1']
-            siap['MASA'] = int(masa)
+            ws = self.wb['N.5.1']
+
             
             for r in dataframe_to_rows(siap, index=True, header=False):
                 ws.append(r)
