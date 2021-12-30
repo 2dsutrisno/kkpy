5799546e961b5613a53f68d59ba1b85db49d0229
2dsutrisno <2d.sutrisno@gmail.com>
2021-12-30 18:20:31 +0700

    Add files via upload
    Sampai impor berjalan lancar

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ def main():
     # masa_akhir = input('Masa Akhir :')
     
     Test = kkpy.KKPy('010619096057000', '01', '12', '2020')
+
     for i in range(int(Test.masa_awal), int(Test.masa_akhir) + 1):
-        Test.a1_to_n21(str(i))
-
-    for i in range(int(Test.masa_awal),int(Test.masa_akhir) + 1 ):
-        Test.a2_to_n31(str(i))
-
+       Test.copy_ekspor(str(i))
+       Test.copy_pungut_sendiri(str(i))
+       Test.copy_dipungut_pemungut(str(i))
+       Test.copy_tdk_dipungut(str(i))
+       Test.copy_dibebaskan(str(i))
+       Test.copy_impor(str(i))
     Test.save("KKP Baru.xlsx")
 
 
