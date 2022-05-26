68d6fe2f63dbb93c2d7e2468ef3ad790623bbc9a
Dwi Sutrisno <2d.sutrisno@gmail.com>
2022-05-26 11:34:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     # masa_awal = input('Masa Awal :')
     # masa_akhir = input('Masa Akhir :')
     
-    Test = kkpy.KKPy('010619096057000', '01', '12', '2020')
+    Test = kkpy.KKPy('012345678057000', '01', '12', '2020')
 
     for i in range(int(Test.masa_awal), int(Test.masa_akhir) + 1):
        Test.copy_ekspor(str(i))
